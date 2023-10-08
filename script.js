f67788e6060c048e20853a2c6f526221e13b3c9a
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-08 11:34:02 +0200

    Exercice 2 corrigé

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const listePhrases  = ["Pas de panique !", "La vie, l'univers et le reste", "Mer
 let score = 0
 
 let choixUtilisateur = prompt("Voulez vous des 'mots' ou des 'phrases' ?")
-while (choixUtilisateur !== "mots" & choixUtilisateur !== "phrases") {
+while (choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
     choixUtilisateur = prompt("Désolé, je n'ai pas compris. Voulez vous des 'mots' ou des 'phrases' ?")
 }
 
 if (choixUtilisateur === "mots") {
-    for (let i = 0; i < 3; i++ ) {
+    for (let i = 0; i < listeMots.length; i++ ) {
         let motUtilisateur = prompt("entre une le mot : " + listeMots[i])
         if (motUtilisateur === listeMots[i]) {
         score++ }
@@ -16,7 +16,7 @@ if (choixUtilisateur === "mots") {
 }
 else {
     if (choixUtilisateur === "phrases") {
-        for (let i = 0; i < 3; i++ ) {
+        for (let i = 0; i < listePhrases.length; i++ ) {
             let motUtilisateur = prompt("entre une le mot : " + listePhrases[i])
             if (motUtilisateur === listePhrases[i]) {
             score++ }
