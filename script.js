46857edb070339f3200f17d1e9b1238255854360
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-08 10:45:39 +0200

    Exercice 1

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+let motUtilisateur = prompt("entre une le mot : " + listeMots[0])
+
+if (motUtilisateur === listeMots[0]) {
+    score++
+    console.log("GG bro, tu gagnes un pountos.")
+    console.log("Score : " + score)  }
+else {
+    console.log("Dommage ! Tu feras mieux au prochain mot.")
+    console.log("Score : " + score) }
+
+motUtilisateur = prompt("entre une le mot : " + listeMots[1])
+
+if (motUtilisateur === listeMots[1]) {
+    score +=1
+    console.log("GG bro, tu gagnes un pountos.")
+    console.log("Score : " + score)  }
+else {
+    console.log("Dommage ! Tu feras mieux au prochain mot.")
+    console.log("Score : " + score) }
+
+    motUtilisateur = prompt("entre une le mot : " + listeMots[2])
+
+if (motUtilisateur === listeMots[2]) {
+    score +=1
+    console.log("GG bro, tu gagnes un pountos.")
+    console.log("Score : " + score)  }
+else {
+    console.log("Dommage ! Tu feras mieux au prochain mot.")
+    console.log("Score : " + score) }
