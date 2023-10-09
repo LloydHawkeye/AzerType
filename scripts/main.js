9a60fe8a0752f3f57271ef466e48af18984a4dc7
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-09 16:24:55 +0200

    Exercice 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-lancerJeu()
+// lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+console.log(btnValiderMot)
+
+let zoneProposition = document.querySelector(".zoneProposition")
+console.log(zoneProposition)
+
+let zoneScoreSpan = document.querySelector(".zoneScore span")
+console.log(zoneScoreSpan)
+
+let listeOptionSourceInput = document.querySelectorAll(".optionSource input")
+console.log(listeOptionSourceInput)
