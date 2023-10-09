53fff58dfacd80aa28c7dfac0986d39ccb5d7476
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-09 11:19:20 +0200

    Exercice 2

---
+++ config.js
@@ -0,0 +1,3 @@
+// Liste des mots/phrases qui seront utilisées
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases  = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
