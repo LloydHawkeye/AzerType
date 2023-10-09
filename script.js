b1f686998a335fd1c1c3e3c8ae9bf4a5186dfc72
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-09 14:45:50 +0200

    Refacto + corrigé

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
-// Initialisation du score à 0
-let score = 0
-let nbMots
-let choixUtilisateur
-
 // fonction pour choisir les mots ou les phrases
 function choisirPhrasesOuMots() {
-    choixUtilisateur = prompt("Voulez vous des 'mots' ou des 'phrases' ?")
+    let choixUtilisateur = prompt("Voulez vous des 'mots' ou des 'phrases' ?")
     while (choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
     // si l'utilisateur ne donne pas le choix mots ou phrases alors on repose la question
     choixUtilisateur = prompt("Désolé, je n'ai pas compris. Voulez vous des 'mots' ou des 'phrases' ?")
     }
+    return choixUtilisateur
 }
 
-function lancerBoucleDeJeu(choixUtilisateur) {
-    if (choixUtilisateur === "mots") {
-        nbMots = listeMots.length
-        for (let i = 0; i < listeMots.length; i++ ) {
-            let motUtilisateur = prompt("entre une le mot : " + listeMots[i])
-            if (motUtilisateur === listeMots[i]) {
-            score++ }
-            }
-    }
-    else {
-        //Si l'utilisateur choisi des phrases
-        if (choixUtilisateur === "phrases") {
-            nbMots = listePhrases.length
-            for (let i = 0; i < listePhrases.length; i++ ) {
-                let motUtilisateur = prompt("entre une le mot : " + listePhrases[i])
-                if (motUtilisateur === listePhrases[i]) {
-                score++ }
-                }
+function lancerBoucleDeJeu(listePropositions) {
+    let score = 0
+    for (let i = 0; i < listePropositions.length; i++) {
+        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
+        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
+        if (motUtilisateur === listePropositions[i]) {
+            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
+            score++
         }
     }
+    return score
 }
 
 function afficherResultat (score, nbMots) {
-    let résultat = "Votre score est de " + score + " sur " + nbMots
-    return résultat
+    console.log("Votre score est de " + score + " sur " + nbMots)
 }
 
 function lancerJeu() {
-    choisirPhrasesOuMots()
-    lancerBoucleDeJeu(choixUtilisateur)
+    let choixUtilisateur = choisirPhrasesOuMots()
+    let score = 0
+    let nbMots = 0
+
+    if (choixUtilisateur === "mots") {
+        score = lancerBoucleDeJeu(listeMots)
+        nbMots = listeMots.length
+    } 
+    else {
+        score = lancerBoucleDeJeu(listePhrases)
+        nbMots = listePhrases.length
+    }
+    afficherResultat(score, nbMots)
 }
 
 lancerJeu()
-let scoreFinal = afficherResultat(score, nbMots)
-console.log(scoreFinal)
-
-
-// restitution du score
-// console.log("Score : " + score)
