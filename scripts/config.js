d35482a54283f779e5dadb7853a88cbc40214611
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-13 15:09:45 +0200

    Retrait d'une variable obsolète

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,3 @@
 // Liste des mots/phrases qui seront utilisées
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 const listePhrases  = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
-let i = 0
