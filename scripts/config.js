13a19e2c97e17d8ef133296e6b8df46da58856d0
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-15 16:09:30 +0200

    mise à jour des phrases

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
 // Liste des mots/phrases qui seront utilisées
-const listeMots = ["Mouche", "Triforce", "Chausette", "Objection", "Raclette", "Suave" ]
-const listePhrases  = ["Prêt à travailler", "The cake is a lie", "Attrapez-les tous", "Travail terminé"]
+const listeMots = ["Mouche", "Triforce", "Chausette", "Objection", "Raclette", "Suave"]
+const listePhrases  = ["Prêt à travailler", "The cake is a lie", "Attrapez-les tous", "La cuillère n'existe pas", "Pikachu ! Attaque éclair", "Travail terminé"]
