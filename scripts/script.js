613929a902180b5b6b63eb0a10309c392621761d
LloydHawkeye <nicolas.lange33@gmail.com>
2023-10-15 16:05:03 +0200

    Nettoyage du code

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,3 @@
-// fonction pour choisir les mots ou les phrases
-/* function choisirPhrasesOuMots() {
-    let choixUtilisateur = prompt("Voulez vous des 'mots' ou des 'phrases' ?")
-    while (choixUtilisateur !== "mots" && choixUtilisateur !== "phrases") {
-    // si l'utilisateur ne donne pas le choix mots ou phrases alors on repose la question
-    choixUtilisateur = prompt("Désolé, je n'ai pas compris. Voulez vous des 'mots' ou des 'phrases' ?")
-    }
-    return choixUtilisateur
-} */
-
-/* function lancerBoucleDeJeu(listePropositions) {
-    let score = 0
-    for (let i = 0; i < listePropositions.length; i++) {
-        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
-        let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-        if (motUtilisateur === listePropositions[i]) {
-            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
-            score++
-        }
-    }
-    return score
-} */
-
 function afficherResultat (score, nbMots) {
     let spanScore = document.querySelector(".zone-score span")
     let affichageScore = `${score} / ${nbMots}`
@@ -55,6 +32,7 @@ for (let x = 0; x < listOptionInput.length; x++) {
             // Si le mot saisi par l'utilisateur est correct, on incrémente le score
             score++
             i++
+            saisiUtilisateur.setAttribute("style", "border:none")
             divError.setAttribute("style", "display:none")
         }
         else { if (saisiUtilisateur.value === "") {
